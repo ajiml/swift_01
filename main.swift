ca9f445ac5858c6d76be87ab6b07f019efa7e57e
ajiml <41119112+ajiml@users.noreply.github.com>
2026-04-06 14:37:12 +0800

    original

---
+++ main.swift
@@ -0,0 +1,15 @@
+print("Hello! Swift!")
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+var x = 0.0, y = 0.0, z=0.0
+
+var welcomeMessage:String
+
+welcomeMessage = "Hello!"
+print(welcomeMessage)
+
+//字串插補
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
